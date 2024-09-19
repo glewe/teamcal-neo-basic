db1dbddcb4b9dc42a88d3c6455da0b700ac8d48d
George Lewe <george@lewe.com>
2024-09-19 22:23:58 +0200

    Sample database update

--- src/sql/sample.sql
+++ src/sql/sample.sql
@@ -710,6 +710,7 @@ CREATE TABLE IF NOT EXISTS `tcneo_log` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+
 -- --------------------------------------------------------
 
 --
